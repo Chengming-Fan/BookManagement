15b6d2131052c6733b0796f757d9fea8f84d48b1
chengming.fan <chengming.fan@thoughtworks.com>
2024-04-29 02:12:34 +0800

    feat: add book by calling api

--- app/src/main/java/com/fan/bookmanagement/fragments/AddFragment.kt
+++ app/src/main/java/com/fan/bookmanagement/fragments/AddFragment.kt
@@ -1,13 +1,30 @@
 package com.fan.bookmanagement.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import by.dzmitry_lakisau.month_year_picker_dialog.MonthYearPickerDialog
 import com.fan.bookmanagement.R
+import com.fan.bookmanagement.data.Book
 import com.fan.bookmanagement.databinding.FragmentAddBinding
+import com.google.gson.Gson
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import okio.IOException
 
 class AddFragment : Fragment() {
 
@@ -42,10 +59,73 @@ class AddFragment : Fragment() {
                 dialog.show()
             }
         }
+        binding.buttonAdd.isEnabled = false
+        binding.edittextTitle.requestFocus()
+        val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(binding.edittextTitle, InputMethodManager.SHOW_IMPLICIT)
+
+        binding.edittextTitle.addTextChangedListener(textWatcher)
+        binding.edittextAuthor.addTextChangedListener(textWatcher)
+        binding.edittextYear.addTextChangedListener(textWatcher)
+        binding.edittextIsbn.addTextChangedListener(textWatcher)
+
+        binding.buttonAdd.setOnClickListener {
+            val title = binding.edittextTitle.text.trim().toString()
+            val author = binding.edittextAuthor.text.trim().toString()
+            val year = binding.edittextYear.text.trim().toString()
+            val isbn = binding.edittextIsbn.text.trim().toString()
+            val book = Book(title, author, year.toInt(), isbn)
+            addBook(book)
+
+            val navController = Navigation.findNavController(it)
+            navController.navigateUp()
+
+            imm.hideSoftInputFromWindow(it.windowToken, 0)
+        }
     }
+
+    private fun addBook(book: Book) {
+        val client = OkHttpClient()
+
+        val requestBody =
+            Gson().toJson(book).toRequestBody("application/json".toMediaTypeOrNull())
+        val request = Request.Builder()
+            .addHeader("Content-Type", "application/json")
+            .url("http://192.168.0.100:8080/books")
+            .post(requestBody)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Looper.prepare()
+                Toast.makeText(context, "Failed to call API, please try later", Toast.LENGTH_LONG).show()
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+                }
+            }
+        })
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private val textWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {}
+        override fun beforeTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            val title = binding.edittextTitle.text.trim()
+            val author = binding.edittextAuthor.text.trim()
+            val year = binding.edittextYear.text.trim()
+            val isbn = binding.edittextIsbn.text.trim()
+            binding.buttonAdd.isEnabled = title.isNotEmpty() && author.isNotEmpty() &&
+                    year.isNotEmpty() && isbn.isNotEmpty()
+        }
     }
 }
