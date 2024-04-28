9869bee75dafac686ea58e373e51d80f73bfed8e
chengming.fan <chengming.fan@thoughtworks.com>
2024-04-28 12:39:34 +0800

    feat: add recycler view and floating button for ListFragment

--- app/src/main/java/com/fan/bookmanagement/fragments/ListFragment.kt
+++ app/src/main/java/com/fan/bookmanagement/fragments/ListFragment.kt
@@ -1,22 +1,18 @@
-package com.fan.bookmanagement
+package com.fan.bookmanagement.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.fan.bookmanagement.databinding.FragmentSecondBinding
+import com.fan.bookmanagement.R
+import com.fan.bookmanagement.databinding.FragmentListBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class SecondFragment : Fragment() {
+class ListFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentListBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -24,7 +20,7 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,8 +28,8 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        binding.floatingAddButton.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
 
