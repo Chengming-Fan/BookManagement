a3816caaba871152b8eebfc7a388b11d4b210eb9
chengming.fan <chengming.fan@thoughtworks.com>
2024-04-28 15:06:07 +0800

    feat: add search input in app bar

--- app/src/main/java/com/fan/bookmanagement/fragments/ListFragment.kt
+++ app/src/main/java/com/fan/bookmanagement/fragments/ListFragment.kt
@@ -2,16 +2,22 @@ package com.fan.bookmanagement.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.fan.bookmanagement.R
 import com.fan.bookmanagement.adapters.BookListAdapter
 import com.fan.bookmanagement.databinding.FragmentListBinding
 
-class ListFragment : Fragment() {
+class ListFragment : Fragment(), MenuProvider {
 
     private var _binding: FragmentListBinding? = null
 
@@ -21,6 +27,8 @@ class ListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
 
         _binding = FragmentListBinding.inflate(inflater, container, false)
         return binding.root
@@ -43,5 +51,18 @@ class ListFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_main, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
+            R.id.app_bar_search -> {
+                true
+            }
+            else -> false
+        }
     }
 }
